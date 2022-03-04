dbea847e73eb1387b4226bc5b33d8151a1aece3f
BrandonComins <brandcomin@gmail.com>
2022-03-03 19:54:25 -0800

    clean comments and formating

--- src/main/java/com/team4/robot/Constants.java
+++ src/main/java/com/team4/robot/Constants.java
@@ -1,7 +1,7 @@
 package com.team4.robot;
 
 public class Constants {
-		// TODO: What is this for?
+		
     public static final int kCANTimeoutMs = 10;
     public static final int kLongCANTimeoutMs = 100;
 
@@ -9,74 +9,57 @@ public class Constants {
     public static final int kDriverControlSlot = 0;
     public static final int kOperatorControlSlot = 1;
 
-	//CHANGE IN CODE: ID #'s
     // Hardware
     public static final int kDriveLeftMaster1 = 3;
     public static final int kDriveLeftFollower2 = 1;
     public static final int kDriveRightMaster1 = 4;
     public static final int kDriveRightFollower2 = 2;
-		public static final int kShooterMaster1 = 5;
-		public static final int kShooterFollower2 = 6;
-		public static final int kRollerMotorID = 7;
-		public static final int kArmMotorID = 0;
-		// TODO: Are these Master/Slave?
+	public static final int kShooterMaster1 = 5;
+	public static final int kShooterFollower2 = 6;
+	public static final int kRollerMotorID = 7;
+	public static final int kArmMotorID = 0;
 
 
-		// TODO: figure the motor configuration for conveyor
-		//public static final int kConveyorFirstStageRight = 8;
-		//public static final int kConveyorFirstStageLeft = 9;
-		//public static final int kHopperMaster = 10;
-		//public static final int kConveyorFinalStageBottom = 11;
-		//public static final int kConveyorFinalStageTop = 12;
+	public static final int kIntakeFirst = 8;
+	public static final int kConveyor = 10;
 
-		public static final int kIntakeFirst = 8;
-		public static final int kConveyor = 10;
-
+	public static final int kClimberLeftMotor = 13;
+	public static final int kClimberRightMotor = 14;
+	
+	public static final int kIntakeSolenoidLeft = 0;
+	public static final int kIntakeSolenoidRight = 1;
 
 
 
-		public static final int kClimberLeftMotor = 13;
-		//public static final int kClimberFollower1 = ;
-		public static final int kClimberRightMotor = 14;
-		//public static final int kClimberWinchRight = ;
-
-
-
-		public static final int kIntakeSolenoidLeft = 0;
-		public static final int kIntakeSolenoidRight = 1;
-
-
-
-		// Drivetrain Dynamics
+	// Drivetrain Dynamics
     public static final double kDriveWheelTrackWidthInches = 32;
     public static final double kDriveWheelDiameterInches = 6;
     public static final double kDriveWheelCircumferenceInches = Math.PI * kDriveWheelDiameterInches;
     public static final double kDriveWheelRadiusInches = kDriveWheelDiameterInches / 2.0;
-    public static final double kDriveTrackScrubFactor = 1.0;  // Tune me!
+    public static final double kDriveTrackScrubFactor = 1.0; 
     public static final double kDriveGearRatio = 1/7.29; //On actual bot 12d / 48d, 7.71/1  1/7.71, .129701
 
-		// Shooter
-		// TODO
-		public static final double kShooterTicksPerRevolution = 2048; // based on gear reduction between encoder and output shaft, and encoder ppr
+	// Shooter
+	public static final double kShooterTicksPerRevolution = 2048; // based on gear reduction between encoder and output shaft, and encoder ppr
 
-		public static final double kShooterMomentOfInertia = 0.003440853333; // kg * m^2
-		public static final double kShooterGearRatio = 1.0; // >1 if flywheel geared down
+	public static final double kShooterMomentOfInertia = 0.003440853333; // kg * m^2
+	public static final double kShooterGearRatio = 1.0; // >1 if flywheel geared down
 
-		// Robot Dynamics
-		public static final double kRobotMOI = 6; // Moment of inertia of the robot around the center
-		public static final double kRobotMass = 56; // Robot Mass (kg)
+	// Robot Dynamics
+	public static final double kRobotMOI = 6; // Moment of inertia of the robot around the center
+	public static final double kRobotMass = 56; // Robot Mass (kg)
 
-		public static final double kJoystickThreshold = 0.04;
+	public static final double kJoystickThreshold = 0.04;
 
-		public static final double kLoopTime = 0.020;
+	public static final double kLoopTime = 0.020;
 
-		public static final int kCompressorID = 1;
+	public static final int kCompressorID = 1;
 
-		public static final double kShooterKp = 1.1;
-		public static final double kShooterKi = 0.0;
-		public static final double kShooterKd = 1.25;
-		public static final double kShooterKf = .0725;
-		public static final int kShooterIZone = 0;
-		public static final double kShooterRampRate = 0.0;
-		public static final double kShooterEnconderPPR = 2048.0;
+	public static final double kShooterKp = 1.1;
+	public static final double kShooterKi = 0.0;
+	public static final double kShooterKd = 1.25;
+	public static final double kShooterKf = .0725;
+	public static final int kShooterIZone = 0;
+	public static final double kShooterRampRate = 0.0;
+	public static final double kShooterEnconderPPR = 2048.0;
 }
