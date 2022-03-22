f89314562d03a6cfd30f8459db44b43197c00664
BrandonComins <brandcomin@gmail.com>
2022-03-21 18:55:41 -0700

    remove comments in robot

--- src/main/java/com/team4/robot/Robot.java
+++ src/main/java/com/team4/robot/Robot.java
@@ -23,22 +23,12 @@ public class Robot extends TimedRobot {
 	public static Shooter mShooter = new Shooter();
   public static Conveyor mConveyor = new Conveyor();
   public static Climber mClimber = new Climber();
+  
+  public static Compressor mCompressor = new Compressor(Constants.kCompressorID, PneumaticsModuleType.CTREPCM);
   DriveHelper mDriveHelper = DriveHelper.getInstance();
-
   TeleopControls mTeleopControls = new TeleopControls();
-
-  // Controllers
-
-	public static Compressor mCompressor = new Compressor(Constants.kCompressorID, PneumaticsModuleType.CTREPCM);
   AutoExecutor mAutoExecutor = new AutoExecutor();
 
-  
-	
-
-  /**
-   * Entered when the robot first starts up.
-   * Example Usage: Initialization of any subsystems
-   */
   @Override
   public void robotInit() {
     mSubsystemManager.setSubsystems(
@@ -50,12 +40,6 @@ public class Robot extends TimedRobot {
     );
   }
 
-  /**
-   * This is run periodically every loop cycle, regardless of the mode.
-   * Example Usage: Diagnostics to be run for each mode.
-   * Details: THis is run AFTER the mode specific periodic method, but before
-   * SmartDashboard is updated.
-   */
   @Override
   public void robotPeriodic() {
   }
@@ -67,22 +51,11 @@ public class Robot extends TimedRobot {
     mSubsystemManager.onEnabledStart();
   }
 
-  /**
-   * This is run periodically when Autonomous is running.
-   * Example Usage: Any periodic updates to the robot that should happen while
-   * Autonomous is running.
-   * Details: If using the default Notifier, then periodic robot calls should be
-   * placed here.
-   */
   @Override
   public void autonomousPeriodic() { 
     mSubsystemManager.onEnabledLoop();
   }
 
-  /**
-   * This is run once when Teleop is entered.
-   * Example Usage: Should be used to start any subsystems.
-   */
   @Override
   public void teleopInit() {
     System.out.println("Teleop Init!");
@@ -92,23 +65,13 @@ public class Robot extends TimedRobot {
     mSubsystemManager.onEnabledStart();
   }
 
-  /**
-   * This is run periodically when Teleop is running.
-   * Example Usage: Modify Subsystem behavior based on input from the Driver
-   * Example Usage: Run each Subsystem's periodic behavior.
-   */
   @Override
   public void teleopPeriodic() {
     // Controller Inputs
     mTeleopControls.runTeleop();
     mSubsystemManager.onEnabledLoop();
-    }
+  }
   
-
-  /**
-   * This is run once when the robot is disabled.
-   * Example Usage: Reset Auto Mode, run any disabled behavior for subsystems
-   */
   @Override
   public void disabledInit() {
     mAutoExecutor.stop();
@@ -116,29 +79,18 @@ public class Robot extends TimedRobot {
     mSubsystemManager.onDisabledStart();
   }
 
-  /**
-   * This is run periodically when the robot is disabled.
-   * Example Usage: Update Auto Modes, reset any subsystems.
-   */
   @Override
   public void disabledPeriodic() {
     mSubsystemManager.onDisabledLoop();
     mAutoExecutor.setAutoMode(new DoNothingMode());
   }
 
-  /**
-   * This is run once when Test is entered.
-   * Example Usage: Should be used to run tests on any subsystems.
-   */
   @Override
   public void testInit() {
     mSubsystemManager.onEnabledStop();
     mSubsystemManager.onDisabledStop();
   }
 
-  /**
-   * This is run periodically when Test is running.
-   */
   @Override
   public void testPeriodic() {
   }
