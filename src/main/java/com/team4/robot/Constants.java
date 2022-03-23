a59ba084bd3454a7dd633f536c383918c265a79e
Matthew Fulde <matthew.fulde@ht-la.org>
2022-03-22 21:04:02 -0700

    add missing constants from test branch

--- src/main/java/com/team4/robot/Constants.java
+++ src/main/java/com/team4/robot/Constants.java
@@ -8,18 +8,39 @@ public class Constants {
     // Controllers
     public static final int kDriverControlSlot = 0;
     public static final int kOperatorControlSlot = 1;
+	public static final double kJoystickThreshold = 0.1;
 
     // Drive Motors
     public static final int kDriveLeftMaster1 = 3;
     public static final int kDriveLeftFollower2 = 1;
     public static final int kDriveRightMaster1 = 4;
     public static final int kDriveRightFollower2 = 2;
+
+	// Drivetrain Dynamics
+	public static final double kDriveWheelTrackWidthInches = 32;
+	public static final double kDriveWheelDiameterInches = 6;
+	public static final double kDriveWheelCircumferenceInches = Math.PI * kDriveWheelDiameterInches;
+	public static final double kDriveWheelRadiusInches = kDriveWheelDiameterInches / 2.0;
+	public static final double kDriveTrackScrubFactor = 1.0; 
+	public static final double kDriveGearRatio = 1/6.43;
+	public static final double kDriveEnconderPPR = 2048.0;
+
+	// Drive PID values
+	public static final double kDriveDistanceKP = 0.0;
+	public static final double kDriveDistanceKI = 0.0;
+	public static final double kDriveDistanceKD = 0.0;
+		
+	public static final double kDriveAngleKP = 0.0;
+	public static final double kDriveAngleKI = 0.0;
+	public static final double kDriveAngleKD = 0.0;
+	
+	public static final double kDistancePidTolerance = 1.0;
 	
 	// Shooter Motors
 	public static final int kShooterBottom = 6; 
 	public static final int kShooterTop = 5; 
 	
-	//Intake Motors
+	// Intake Motors
 	public static final int kRollerMotorID = 7;
 	public static final int kArmMotorID = 0;
 	public static final int kIntakeFirst = 8;
@@ -28,50 +49,17 @@ public class Constants {
 	public static final int kConveyorLeft = 10;
 	public static final int kConveyorRight = 11;
 
-	//Climber Motors
+	// Climber Motors
 	public static final int kClimberLeftMotor = 13;
 	public static final int kClimberRightMotor = 14;
 	
-
-	//Pnuematics
+	// Pnuematics
 	public static final int kIntakeSolenoidLeft = 7;
 	public static final int kIntakeSolenoidRight = 4;
 	public static final int kClimbSolenoidLeft = 6;
     public static final int kClimbSolenoidRight = 5;
 	public static final int kCompressorID = 1;
-
-	//Drive PID values
-	public static final double kDriveDistanceKP = 0.0;
-	public static final double kDriveDistanceKI = 0.0;
-	public static final double kDriveDistanceKD = 0.0;
 	
-	public static final double kDriveAngleKP = 0.0;
-	public static final double kDriveAngleKI = 0.0;
-	public static final double kDriveAngleKD = 0.0;
-
-	public static final double kDistancePidTolerance = 1.0;
-	
-
-	// Drivetrain Dynamics
-    public static final double kDriveWheelTrackWidthInches = 32;
-    public static final double kDriveWheelDiameterInches = 6;
-    public static final double kDriveWheelCircumferenceInches = Math.PI * kDriveWheelDiameterInches;
-    public static final double kDriveWheelRadiusInches = kDriveWheelDiameterInches / 2.0;
-    public static final double kDriveTrackScrubFactor = 1.0; 
-    public static final double kDriveGearRatio = 1/6.43;
-	public static final double kDriveEnconderPPR = 2048.0;
-
-	// Shooter
-	public static final double kHighVelocityBottom = 3700;
-	public static final double kHighVelocityTop = 3500;
-	public static final double kLowVelocityBottom = 600; 
-	public static final double kLowVelocityTop = 400;
-	public static final double kShooterTicksPerRevolution = 2048; 
-
-	public static final double kShooterMomentOfInertia = 0.003440853333; // kg * m^2
-	public static final double kShooterBottomGearRatio = 48.0/32.0; 
-	public static final double kShooterTopGearRatio = 50.0/30.0;
-
 	// Intake
 	public static final double kIntakeForwardPower = 0.75;
 	public static final double kIntakeReversePower = -0.75;
@@ -82,11 +70,14 @@ public class Constants {
 	public static final double kConveyorReversePower = -0.35;
 	public static final double kConveyorOff = 0;
 
-	// Robot Dynamics
-	public static final double kRobotMOI = 6; // Moment of inertia of the robot around the center
-	public static final double kRobotMass = 56; // Robot Mass (kg)
-	public static final double kJoystickThreshold = 0.1;
-	public static final double kLoopTime = 0.020;
+	// Shooter
+	public static final double kHighVelocityBottom = 1400;
+	public static final double kHighVelocityTop = 1200;
+	public static final double kLowVelocityBottom = 600; 
+	public static final double kLowVelocityTop = 400;
+
+	public static final double kShooterBottomGearRatio = 48.0/32.0; 
+	public static final double kShooterTopGearRatio = 50.0/30.0;
 
 	//Shooter PID
 	public static final double kShooterBottomKp = 0.115;
@@ -98,10 +89,8 @@ public class Constants {
 	public static final double kShooterTopKi = 0.0;
 	public static final double kShooterTopKd = 5.5;
 	public static final double kShooterTopKf = .046;
-
-
+	
 	public static final int kShooterIZone = 0;
 	public static final double kShooterRampRate = 0.0;
 	public static final double kShooterEnconderPPR = 2048.0;
-
 }
