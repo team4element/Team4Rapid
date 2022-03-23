dfe015df3387dd0419286e5b186853c65dafa0b4
Matt F <mpfulde@ucdavis.edu>
2022-03-23 15:47:14 -0700

    add test paths and a test auto mode

---
+++ src/main/java/com/team4/robot/paths/TestCurvePath.java
@@ -0,0 +1,27 @@
+package com.team4.robot.paths;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.team254.lib.geometry.Pose2d;
+import com.team254.lib.geometry.Rotation2d;
+import com.team4.lib.trajectory.SimpleTrajectory;
+import com.team4.lib.trajectory.Waypoint;
+
+public class TestCurvePath extends SimpleTrajectory {
+
+    @Override
+    public Waypoint points() {
+        List<Pose2d> points = new ArrayList<>();
+        points.add(new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0)));
+        points.add(new Pose2d(120.0, 90.0, Rotation2d.fromDegrees(90.0)));
+
+        return Waypoint.createWaypointFromPoints(false, points);
+    }
+
+    @Override
+    public Pose2d startPose() {
+        return new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
+    }
+    
+}
